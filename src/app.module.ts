60786ecda3089b49abfa501cde28622a60af33ce
0xlocal <ferreluchiha@gmail.com>
2021-11-05 14:29:12 +0700

    add config, database, and user module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
+import { UsersModule } from './users/users.module';
+import { RecipesModule } from './recipes/recipes.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+    RecipesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
