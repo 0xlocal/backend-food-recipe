5091ebcd3c300f619d858a5ed7abc5e590515535
0xlocal <ferreluchiha@gmail.com>
2021-11-09 19:35:48 +0700

    added routes using nest router

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
 import { UsersModule } from './users/users.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { RecipeCategoriesModule } from './recipe-categories/recipe-categories.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
+import { IngredientCategoriesModule } from './ingredient-categories/ingredient-categories.module';
+import { RouterModule } from '@nestjs/core';
+import routes from './routes';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    RouterModule.register(routes),
     DatabaseModule,
     UsersModule,
     RecipesModule,
+    RecipeCategoriesModule,
+    IngredientsModule,
+    IngredientCategoriesModule,
   ],
   controllers: [],
   providers: [],
